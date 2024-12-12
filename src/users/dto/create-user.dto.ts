7641a7654f6dd73e40d5d8f8b5e34e34472636a9
CauanFelipeTavares <tavarescauanf@gmail.com>
2024-12-12 19:38:04 -0300

    first class

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator"
+
+export class CreateUserDto {
+    @IsString({ message: 'O nome precisa ser uma string' })
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Email precisa ser do tipo Email' })
+    @IsEmail()
+    email: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+}
